0336bc8c455d157b9ec991dbc3b717e3f1c34f30
sechyss <albertotr92@gmail.com>
2024-10-21 13:48:50 +0100

    Push

--- Scripts/Python_scripts/Rename_taxa.py
+++ Scripts/Python_scripts/Rename_taxa.py
@@ -13,13 +13,13 @@ for index, row in dataset_keys.iterrows():
 sorted_replacements = dict(sorted(replacements.items(), key=lambda x: len(x[0]), reverse=True))
 
 # Change directory to where the .contree files are located
-os.chdir('/home/albertotr/OneDrive/Data/Cambridge_Project/VCF_trees/')
+os.chdir('/home/albertotr/OneDrive/Data/Cambridge_Project/Mapped_output_Rlow/')
 
 # Iterate over all files ending with .contree in the directory
 for filename in os.listdir():
-    if filename.endswith('.treefile'):
+    if filename.endswith('onlyLucy.fasta'):
         # Construct the new filename with 'Edited_' prefix
-        new_filename = 'Aln_' + filename
+        new_filename = 'Edited_' + filename
 
         # Open the current file and create a new file with the edited name
         with open(filename, 'r') as infile, open(new_filename, 'w') as outfile:
@@ -27,6 +27,8 @@ for filename in os.listdir():
             for line in infile:
                 # Replace each key in the dictionary with its corresponding value
                 for old_string, new_string in sorted_replacements.items():
-                    line = line.replace(old_string, new_string)
+                    if old_string in line:
+                        line = line.replace(old_string, new_string)
+                        break  # Stop searching for more keys after the first match
                 # Write the modified line to the output file
                 outfile.write(line)
