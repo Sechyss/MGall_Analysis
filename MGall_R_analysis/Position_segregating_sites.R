963abdbbf8bc2dd91a5e45ff2b059ec3b094c818
sechyss <albertotr92@gmail.com>
2024-11-01 09:42:25 +0000

    Push

--- MGall_R_analysis/Position_segregating_sites.R
+++ MGall_R_analysis/Position_segregating_sites.R
@@ -5,16 +5,25 @@ setwd("C:/Users/at991/OneDrive - University of Exeter/Data/Cambridge_Project/Map
 
 # Path to the DNA alignment file (FASTA format or other readable by ape)
 alignment_file <- "Rlow_consensus_strains_onlyLucy.fasta"
+lucy_alignment <- "../../VCF_trees/Myco_py.fasta"
 # Read the DNA alignment
 alignment <- read.dna(alignment_file, format = "fasta")
-
+alignment_lucy <- read.dna(lucy_alignment, format = "fasta")
 # Identify segregating sites
 segregating_sites <- seg.sites(alignment)
+segregating_sites_lucy <- seg.sites(alignment_lucy)
 
-# Report positions in the alignment that contain segregating sites
-if (length(segregating_sites) > 0) {
-  cat("Positions with segregating sites:\n")
-  print(segregating_sites)
-} else {
-  cat("No segregating sites found.\n")
-}
+# Convert lists to vectors
+vec1 <- unlist(segregating_sites)
+vec2 <- unlist(segregating_sites_lucy)
+
+# Find common elements
+common_elements <- intersect(vec1, vec2)
+print(common_elements)
+
+# Find elements unique to each list
+unique_to_list1 <- setdiff(vec1, vec2)
+unique_to_list2 <- setdiff(vec2, vec1)
+
+print(unique_to_list1)
+print(unique_to_list2)
