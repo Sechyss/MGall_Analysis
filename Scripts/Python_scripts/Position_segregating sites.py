adb58ae87e948f62f896c1ed5b7f7087c86875e3
sechyss <albertotr92@gmail.com>
2024-11-01 10:15:35 +0000

    Push

--- Scripts/Python_scripts/Position_segregating sites.py
+++ Scripts/Python_scripts/Position_segregating sites.py
@@ -1,14 +1,32 @@
-import allel
+from Bio import AlignIO
 import numpy as np
 
-vcf_file = '/home/albertotr/OneDrive/Data/Cambridge_Project/Mapped_output_Rlow/Only_SNPs/A1.bam.vcf.gz'
-genotypes = allel.read_vcf(vcf_file)['calldata/GT']
+# Define paths to alignment files
+alignment_file = "/home/albertotr/OneDrive/Data/Cambridge_Project/Mapped_output_Rlow/Only_SNPs/Rlow_consensus_strains_onlyLucy.fasta"
+lucy_alignment_file = "/home/albertotr/OneDrive/Data/Cambridge_Project/VCF_trees/Myco_py.fasta"
 
-# Identify segregating sites (polymorphic sites)
-# A site is segregating if it has more than one allele present across samples
-segregating_sites = (genotypes.max(axis=1) > 0) & (genotypes.min(axis=1) == 0)
+# Load alignments
+alignment = AlignIO.read(alignment_file, "fasta")
+alignment_lucy = AlignIO.read(lucy_alignment_file, "fasta")
 
-# Print segregating site positions (True indicates segregating site)
-for row in segregating_sites:
-    if True in row:
-        print(row)
+def get_segregating_sites(alignment_input):
+    """Identify segregating sites in an alignment."""
+    segregating_sites = []
+    for i in range(alignment_input.get_alignment_length()):
+        column = set(record.seq[i] for record in alignment_input)
+        if len(column) > 1:
+            segregating_sites.append(i)
+    return np.array(segregating_sites)
+
+# Identify segregating sites
+segregating_sites_1 = get_segregating_sites(alignment)
+segregating_sites_2 = get_segregating_sites(alignment_lucy)
+
+# Find common and unique elements
+common_elements = np.intersect1d(segregating_sites_1, segregating_sites_2)
+unique_to_list1 = np.setdiff1d(segregating_sites_1, segregating_sites_2)
+unique_to_list2 = np.setdiff1d(segregating_sites_2, segregating_sites_1)
+
+print("Common elements:", common_elements)
+print("Unique to list 1:", unique_to_list1)
+print("Unique to list 2:", unique_to_list2)
