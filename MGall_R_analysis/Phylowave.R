b2d39eae09065e25cb8a68ccd3299e09ce99e3a6
sechyss <albertotr92@gmail.com>
2024-11-12 14:36:51 +0000

    Push

---
+++ MGall_R_analysis/Phylowave.R
@@ -0,0 +1,26 @@
+library(ape, quiet = T); library(phytools, quiet = T); library(stringr, quiet = T)
+library(MetBrewer, quiet = T); library(parallel, quiet = T); library(mgcv, quiet = T)
+library(cowplot, quiet = T); library(ggplot2, quiet = T); library(ggtree, quiet = T);
+library(cmdstanr, quiet = T); library(binom, quiet = T)
+
+setwd("C:/Users/at991/OneDrive - University of Exeter/Data/Cambridge_Project/Mapped_output_Rlow/Only_SNPs")
+
+timed_tree = read.nexus('./Rlow_consensus_snps_trimmed_nogaps.masked.finaltree.nex')
+
+## Names all sequences
+names_seqs = timed_tree$tip.label
+n_seq = length(names_seqs)
+## Collection times of all sequences
+times_seqs = as.numeric(sapply(names_seqs, function(x)tail(str_split(x, pattern = '/')[[1]],2)[1]))
+## Nextstrain clades of all sequences
+clades_seqs = sapply(names_seqs, function(x)tail(str_split(x, pattern = '/')[[1]],1))
+
+## Pairwise distance matrix
+genetic_distance_mat = dist.nodes.with.names(timed_tree)
+
+## Time for each internal node
+
+nroot = length(timed_tree$tip.label) + 1 ## Root number
+distance_to_root = genetic_distance_mat[nroot,]
+root_height = times_seqs[which(names_seqs == names(distance_to_root[1]))] - distance_to_root[1]
+nodes_height = root_height + distance_to_root[n_seq+(1:(n_seq-1))]
