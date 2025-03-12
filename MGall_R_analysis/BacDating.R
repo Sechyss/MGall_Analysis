9b8f6fd4d0dd894b2e94c61d839f71bb33a24790
sechyss <albertotr92@gmail.com>
2025-03-12 09:06:54 +0000

    Changes to bacdating

--- MGall_R_analysis/BacDating.R
+++ MGall_R_analysis/BacDating.R
@@ -3,6 +3,11 @@ library(ape)
 setwd("C:/Users/at991/OneDrive - University of Exeter/Data/Cambridge_Project/Pangenome_results_HF/Iqtree_phylogenies/")
 
 # Load the tree
-tree=read.tree("core-genome_HF.timetree.nex")
+tree=read.tree("core-genome_HF.treefile")
 plot(tree)
 ape::axisPhylo(backward=F, root.time = 1990)
+obsphy <- simobsphy(tree)
+
+# Plot the root-to-tip regression
+res <- bactdate(obsphy, 1990:2015)
+plot(res, 'treeCI')
