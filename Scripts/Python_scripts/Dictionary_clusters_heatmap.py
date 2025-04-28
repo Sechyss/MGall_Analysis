80ad6cc282ea546450dc3fa451a5d90d447c76d5
Sechyss <a.torcello-requena@exeter.ac.uk>
2025-04-28 09:39:50 +0100

    Refactor clustering script to include Cas9 genes and update motility gene handling; save updated clusters to a new pickle file.

--- Scripts/Python_scripts/Dictionary_clusters_heatmap.py
+++ Scripts/Python_scripts/Dictionary_clusters_heatmap.py
@@ -18,12 +18,18 @@ filtered_presence_absence = presence_absence_file[
 cluster_lipoproteins = filtered_presence_absence['Gene'].unique()
 # Create a dictionary to store the clusters and their corresponding lipoproteins
 cluster_dict = {'Lipoprotein' : cluster_lipoproteins}
-with open(f'{base_path}/Lineage_differences/Lipoprotein/HMM_scan/lipoprotein_clusters.pickle', 'wb') as handle:
-    pickle.dump(cluster_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
-# %%
 
-base_path = '/home/albertotr/OneDrive/Data/Ipoutcha_motility/BLAST_db'
-blastp_db = pd.read_table(f'{base_path}/candidates_motility_pangenome.tsv', sep='\t', header=None)
+# Cas9 genes
+
+cas9_genes = presence_absence_file[presence_absence_file['Gene'].str.contains('cas9', na=False)]
+cluster_cas9 = cas9_genes['Gene'].unique()
+# Add Cas9 genes to the dictionary
+cluster_dict.update({'Cas9': cluster_cas9})
+
+# Motility genes
+
+base_path_2 = '/home/albertotr/OneDrive/Data/Ipoutcha_motility/BLAST_db'
+blastp_db = pd.read_table(f'{base_path_2}/candidates_motility_pangenome.tsv', sep='\t', header=None)
 
 motility = blastp_db[1].unique()
 filtered_presence_absence = presence_absence_file[
@@ -32,6 +38,18 @@ filtered_presence_absence = presence_absence_file[
 cluster_motility = filtered_presence_absence['Gene'].unique()
 # Create a dictionary to store the clusters and their corresponding motility genes
 cluster_dict.update({'Motility' : cluster_motility})
-with open(f'{base_path}/candidates_motility_pangenome.pickle', 'wb') as handle:
+
+# %% Check for any redundant genes in the clusters and select only unique ones
+
+unique_to_lipoproteins = set(cluster_dict['Lipoprotein']).difference(cluster_dict['Motility'])
+unique_to_motility = set(cluster_dict['Motility']).difference(cluster_dict['Lipoprotein'])
+
+cluster_dict['Lipoprotein'] = list(unique_to_lipoproteins)
+cluster_dict['Motility'] = list(unique_to_motility)
+
+
+# Save the dictionary to a pickle file
+
+with open(f'{base_path}/candidates_clusters_pangenome.pickle', 'wb') as handle:
     pickle.dump(cluster_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
 # %%
